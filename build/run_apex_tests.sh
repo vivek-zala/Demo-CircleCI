2ffc2f74e091ee64c9876b8a362b400790349749
vivek-zala <vzala@nsforce.in>
2024-11-29 19:09:31 +0530

    Modify project-scratch-def.json

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -6,7 +6,7 @@
 echo "Creating the Scratch Org..."
 sfdx org create scratch -f config/project-scratch-def.json -a ${CIRCLE_BRANCH}
 # sf package install --package "04t3o000001QbwP"
-sfdx force:package:install -p 04t360000011zP6AAI -u ${CIRCLE_BRANCH} -w 5
+sfdx force:package:install -p 04t360000011zP6AAI --target-org ${CIRCLE_BRANCH} -w 5
 sfdx force:source:push -u ${CIRCLE_BRANCH}
 
 #echo "Coverting source to metadata format"
