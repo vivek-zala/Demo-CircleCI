bb614583cb85347c8b7326dfdb4f8057735b706d
vivek-zala <vzala@nsforce.in>
2024-11-29 17:30:52 +0530

    Modify run_sfdx_scanner.sh file

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -1,5 +1,6 @@
 echo "Installing JDK"
 sudo apt-get install openjdk-8-jdk
+sudo update-alternatives --set java /usr/lib/jvm/jdk1.8.0_version/bin/java
 
 echo "Install SFDX Scanner"
 echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
