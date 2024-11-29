960dfab8f75b4d120f057e6a889d977708a5fdd0
vivek-zala <vzala@nsforce.in>
2024-11-29 17:50:51 +0530

    Modify Demo.cls file

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -3,10 +3,10 @@
 #sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
 
 #Create a scratch org
-#echo "Creating the Scratch Org..."
-#sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
+echo "Creating the Scratch Org..."
+sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
 
-#sfdx force:source:push -u ${CIRCLE_BRANCH}
+sfdx force:source:push -u ${CIRCLE_BRANCH}
 
 #echo "Coverting source to metadata format"
 #sfdx force:source:convert -d test_code -r force-app
