330b24c4f01f980e5c0f951b97bd62727623dec1
vivek-zala <vzala@nsforce.in>
2024-12-06 12:42:43 +0530

    Change scratch org name

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -4,4 +4,4 @@ echo $SANDBOX_CERT_KEY | base64 -di > keys/server.key
 
 echo "Authenticating org"
 echo "Using app key, keys, and username"
-sf auth jwt:grant --clientid "$SANDBOX_APP_KEY" --jwtkeyfile keys/server.key --username "$SANDBOX_USERNAME" --setdefaultdevhubusername -a DevHub
+sf auth jwt:grant --clientid "$SANDBOX_APP_KEY" --jwtkeyfile keys/server.key --username "$SANDBOX_USERNAME" --setdefaultdevhubusername -a MyScratchOrg1
