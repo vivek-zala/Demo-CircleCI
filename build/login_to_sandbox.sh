995653a7e7447da76de7da95cd1d0847a4e78257
vivek-zala <vzala@nsforce.in>
2024-12-06 13:49:02 +0530

    Remove ~/.salesforce

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -2,9 +2,13 @@ echo "Logging into Sandbox Salesforce Org"
 mkdir keys
 echo $SANDBOX_CERT_KEY | base64 -di > keys/server.key
 
+echo "Removing previous .salesforce"
+rm -rf ~/.salesforce
+echo "Removed"
+
 echo "Authenticating org"
 echo "Using app key, keys, and username"
-echo "SANDBOX_APP_KEY: $SANDBOX_APP_KEY"
-echo "SANDBOX_USERNAME: $SANDBOX_USERNAME"
+echo "SANDBOX_APP_KEY: SANDBOX_APP_KEY"
+echo "SANDBOX_USERNAME: SANDBOX_USERNAME"
 
-sf auth jwt:grant --clientid "$SANDBOX_APP_KEY" --jwtkeyfile keys/server.key --username "$SANDBOX_USERNAME" --setdefaultdevhubusername -a MyScratchOrg1
+sf auth jwt:grant --clientid "SANDBOX_APP_KEY" --jwtkeyfile keys/server.key --username "SANDBOX_USERNAME" --setdefaultdevhubusername -a MyScratchOrg1
