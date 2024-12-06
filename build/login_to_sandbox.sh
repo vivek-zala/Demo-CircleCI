30dfe55e39d735ec75c6e46c5bce64813b9c314d
vivek-zala <vzala@nsforce.in>
2024-12-06 14:13:16 +0530

    Change JWT sf to sfdx

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -11,4 +11,4 @@ echo "Using app key, keys, and username"
 echo "SANDBOX_APP_KEY: $SANDBOX_APP_KEY"
 echo "SANDBOX_USERNAME: $SANDBOX_USERNAME"
 
-sf auth jwt:grant --clientid "$SANDBOX_APP_KEY" --jwtkeyfile keys/server.key --username "$SANDBOX_USERNAME" --setdefaultdevhubusername -a MyScratchOrg1
+sfdx auth jwt:grant --clientid "$SANDBOX_APP_KEY" --jwtkeyfile keys/server.key --username "$SANDBOX_USERNAME" --setdefaultdevhubusername -a MyScratchOrg1
