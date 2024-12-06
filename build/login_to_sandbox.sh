65ebb4caa4d2b7fbdd1817272fef3e7b5527cf85
vivek-zala <vzala@nsforce.in>
2024-12-06 18:57:56 +0530

    Change defaultdevhub

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -12,7 +12,7 @@ openssl rsa -in keys/server.key -out keys/server.key
 echo "Authenticating org"
 
 # sf auth jwt:grant --clientid "$SANDBOX_APP_KEY" --jwtkeyfile keys/server.key --username "$SANDBOX_USERNAME" --setdefaultdevhubusername -a MyScratchOrg1 --debug
-sf org login jwt --username $USER_NAME --jwt-key-file keys/server.key --client-id $CLIENT_ID -a DevHub --target-dev-hub $USER_NAME
+sf org login jwt --username $USER_NAME --jwt-key-file keys/server.key --client-id $CLIENT_ID -a DevHub --setdefaultdevhubusername
 
 # sf auth jwt:grant --clientid "3MVG9KI2HHAq33RxgA0Kt69Kpii7Jakg3403c0jr.DW1wAzUn3lx8BLoxHoOkyYIvlvVIlJvag50wrrA1dRJ1" --jwt-key-file C:\Users\Vivek\Documents\CertificateGeneration\server.key --username "nilesh.badrakiya@gmail.com" --setdefaultdevhubusername -a MyScratchOrg1
 #
