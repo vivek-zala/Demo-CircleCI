40143efb37cf385e12cbc4c2086912f881c8f2c1
vivek-zala <vzala@nsforce.in>
2024-12-06 15:35:11 +0530

    Remove debug mode

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -7,5 +7,4 @@ mkdir keys
 
 echo "Authenticating org"
 echo "Using app key, keys, and username"
-export SFDX_CLI_DEBUG=true
-sf auth jwt:grant --client-id "$SANDBOX_APP_KEY" --jwt-key-file keys/server.key --username "$SANDBOX_USERNAME" --setdefaultdevhubusername -a MyScratchOrg1 --loglevel DEBUG
+sf auth jwt:grant --client-id "$SANDBOX_APP_KEY" --jwt-key-file keys/server.key --username "$SANDBOX_USERNAME" --setdefaultdevhubusername -a MyScratchOrg1
