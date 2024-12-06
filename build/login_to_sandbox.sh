cdb3531907a4d28718771a981253855178b95943
vivek-zala <vzala@nsforce.in>
2024-12-06 14:44:49 +0530

    Add debug

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -9,4 +9,4 @@ echo $SANDBOX_CERT_KEY | base64 -di > keys/server.key
 echo "Authenticating org"
 echo "Using app key, keys, and username"
 
-sf auth jwt:grant --clientid "$SANDBOX_APP_KEY" --jwtkeyfile keys/server.key --username "$SANDBOX_USERNAME" --setdefaultdevhubusername -a MyScratchOrg1 --debug
+sf auth jwt:grant --clientid "$SANDBOX_APP_KEY" --jwtkeyfile keys/server.key --username "$SANDBOX_USERNAME" --setdefaultdevhubusername -a MyScratchOrg1 --loglevel DEBUG
