95061ecae8c7ff79a36f46eaae2e21346c2cccc5
vivek-zala <vzala@nsforce.in>
2024-12-06 16:05:19 +0530

    Converting server.key to rsa standard

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -2,12 +2,14 @@ echo "Logging into Sandbox Salesforce Org"
 mkdir keys
 echo $SANDBOX_CERT_KEY | base64 -di > keys/server.key
 
+# Ensure the key is in RSA format (if necessary)
+openssl rsa -in keys/server.key -out keys/server.key
+
 # echo "Removing previous .salesforce"
 # rm -rf ~/.salesforce
 # echo "Removed"
 
 echo "Authenticating org"
-echo "Using app key, keys, and username"
 
 # sf auth jwt:grant --clientid "$SANDBOX_APP_KEY" --jwtkeyfile keys/server.key --username "$SANDBOX_USERNAME" --setdefaultdevhubusername -a MyScratchOrg1 --debug
 sf org login jwt --username $USER_NAME --jwtkeyfile keys/server.key --client-id $CLIENT_ID
