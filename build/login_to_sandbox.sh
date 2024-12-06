2998f0cbb85773a1cac2e775e300e23dc78fa60e
vivek-zala <vzala@nsforce.in>
2024-12-06 12:40:55 +0530

    Quoted environment variables

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -3,4 +3,5 @@ mkdir keys
 echo $SANDBOX_CERT_KEY | base64 -di > keys/server.key
 
 echo "Authenticating org"
-sf auth jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub
+echo "Using app key, keys, and username"
+sf auth jwt:grant --clientid "$SANDBOX_APP_KEY" --jwtkeyfile keys/server.key --username "$SANDBOX_USERNAME" --setdefaultdevhubusername -a DevHub
